59ade8f1fa74dd16f849171ca06e3449d8d415e1
DCA-13 <davidcampos1312@gmail.com>
2023-12-07 22:46:59 +0100

    day 2 added id to game list

--- 2023/day2/day2.hs
+++ 2023/day2/day2.hs
@@ -7,23 +7,27 @@ main = do
 
 data Game = End | GameSet Int Int Int Game deriving (Show)
 
-
 storeGame :: String -> Game
 storeGame [] = End
 storeGame x = GameSet red green blue (storeGame ys) where
     xs = last (splitRegex (mkRegex ": ") x)
     z = splitRegex (mkRegex "; ") xs
     y = head z
-    ys = intercalate "; " (tail z) -- falta concatenar los elementos de z [String] -> String
+    ys = intercalate "; " (tail z)
     red = readColor $ matchRegex (mkRegex "([0-9]+) red") y
     green = readColor $ matchRegex (mkRegex "([0-9]+) green") y
     blue = readColor $ matchRegex (mkRegex "([0-9]+) blue") y
-    --readColor :: Maybe [String] -> Int
     readColor Nothing = 0
     readColor (Just x) = read $ head x
 
-readGames :: [String] -> [Game]
-readGames = map storeGame
+idGame :: String -> Int
+idGame [] = 0
+idGame x = readId $ matchRegex (mkRegex "Game ([0-9]+)") x where
+    readId Nothing = 0
+    readId (Just x) = read $ head x
+
+readGames :: [String] -> [(Int,Game)]
+readGames = map (\x -> (idGame x, storeGame x))
 
 maxRed = 12
 maxGreen = 13
@@ -33,7 +37,5 @@ checkGame :: Game -> Bool
 checkGame End = True
 checkGame (GameSet red green blue xs) = (red <= maxRed && green <= maxGreen && blue <= maxBlue) && checkGame xs
 
-checkGames :: [Game] -> [Int]
-checkGames = aux 1 where
-    aux _ [] = []
-    aux n (x:xs) = if checkGame x then n : aux (n + 1) xs else aux (n + 1) xs
+checkGames :: [(Int,Game)] -> [Int]
+checkGames = map fst . filter (checkGame . snd)
