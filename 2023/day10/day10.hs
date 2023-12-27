b40700fd8097f5f26c93e3af5cefa09f0d85a9f3
DCA-13 <davidcampos1312@gmail.com>
2023-12-27 02:39:01 +0100

    day 10 part 1 finished

---
+++ 2023/day10/day10.hs
@@ -0,0 +1,57 @@
+import Control.Monad
+import Data.Map qualified as M
+import System.IO
+
+main = do
+  handle <- openFile "input" ReadMode
+  contents <- hGetContents handle
+  print . solve $ contents
+  -- print . solve2 $ contents
+  hClose handle
+
+type Tiles = M.Map (Int, Int) Char
+
+type Distances = M.Map (Int, Int) Int
+
+n2 = map (\n -> map (n,) [1 ..]) [1 ..]
+
+parseInput :: String -> Tiles
+parseInput = M.fromAscList . filter (\x -> snd x /= '.') . concat . zipWith zip n2 . lines
+
+findStart :: Tiles -> (Int, Int)
+findStart tiles = head . map fst $ filter (\x -> snd x == 'S') (M.assocs tiles)
+
+initDists :: Tiles -> Distances
+initDists tiles = M.singleton (findStart tiles) 0
+
+adjacentPipes :: (Int, Int) -> Char -> [(Int, Int)]
+adjacentPipes (a, b) p
+  | p == '|' = [(a - 1, b), (a + 1, b)]
+  | p == 'J' = [(a - 1, b), (a, b - 1)]
+  | p == 'L' || p == 'S' = [(a - 1, b), (a, b + 1)]
+  | p == '7' = [(a + 1, b), (a, b - 1)]
+  | p == 'F' = [(a + 1, b), (a, b + 1)]
+  | p == '-' = [(a, b - 1), (a, b + 1)]
+
+insertMaybeWith :: (Ord k) => (a -> a -> a) -> k -> Maybe a -> M.Map k a -> M.Map k a
+insertMaybeWith _ _ Nothing map = map
+insertMaybeWith f k (Just x) map = M.insertWith f k x map
+
+insertNewDistances :: (Int, Int) -> [(Int, Int)] -> Distances -> Distances
+insertNewDistances _ [] dists = dists
+insertNewDistances p (x : xs) dists = insertMaybeWith min x ((+ 1) <$> M.lookup p dists) (insertNewDistances p xs dists)
+
+distsIteration :: Tiles -> Distances -> Distances -> Distances
+distsIteration tiles dists acc = M.foldrWithKey insertNewDistances dists . M.mapWithKey adjacentPipes $ M.mapMaybeWithKey (\k _ -> M.lookup k tiles) (M.difference dists acc)
+
+fixAcc :: (Eq a) => (a -> a -> a) -> a -> a -> a
+fixAcc f x acc = if x == f x acc then x else fixAcc f (f x acc) x
+
+fullLoop :: Tiles -> Distances -> Distances
+fullLoop tiles dists = fixAcc (distsIteration tiles) dists M.empty
+
+solve :: String -> Int
+solve input = maximum $ fullLoop tiles dists
+  where
+    tiles = parseInput input
+    dists = initDists tiles
