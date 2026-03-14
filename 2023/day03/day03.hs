1948000cea9009a1104031e8ca1b65910f1df181
DCA-13 <davidcampos1312@gmail.com>
2026-03-14 23:44:47 +0100

    day 3 complete

--- 2023/day03/day03.hs
+++ 2023/day03/day03.hs
@@ -7,15 +7,15 @@ main = do
   handle <- openFile "input" ReadMode
   contents <- hGetContents handle
   let total = sum . solveLines $ lines contents
-  putStr $ show total
+  let total2 = sum . gearRatios . gearParts $ contents
+  print total
+  print total2
   hClose handle
 
 findSymbols :: String -> [Int]
 findSymbols = findIndices isSymbol
   where
-    isSymbol x
-      | isDigit x || x == '.' = False
-      | otherwise = True
+    isSymbol x = not (isDigit x || x == '.')
 
 parseLine :: String -> [(Int, Int, Int)]
 parseLine str = aux 0 str
@@ -29,7 +29,7 @@ parseLine str = aux 0 str
           (read y, start, end) : aux (end + 1) ys
     aux _ _ = []
 
-windows :: [String] -> [[String]]
+windows :: [a] -> [[a]]
 windows (x : y : z : xs) = [x, y, z] : windows (y : z : xs)
 windows _ = []
 
@@ -62,7 +62,42 @@ test =
   \...$.*....\n\
   \.664.598.."
 
--- :: String -> [(Int,Int)] -> Int
--- línea -> coords de nums -> cuántos son diferentes (i.e. dígitos del mismo núm no cuentan)
--- findGears :: String -> [Int]
--- findGears = findIndices (== '*')
+findGears :: (Int, String) -> [(Int, Int)]
+findGears (r, str) = map (r,) (elemIndices '*' str)
+
+solveLine2' :: String -> [(Int, Int)] -> [(Int, Int, Int)]
+solveLine2' str gears = lineGears
+  where
+    line = parseLine str
+    adjacent (r, c) (n, start, end)
+      | c >= start - 1 && c <= end + 1 = [(n, r, c)]
+      | otherwise = []
+    adjacentToGears = map adjacent gears
+    surroundingGears a = concatMap ($ a) adjacentToGears
+    lineGears = concatMap surroundingGears line
+
+solveLine2 :: [(Int, String)] -> [(Int, Int, Int)]
+solveLine2 x = solveLine2' (snd $ x !! 1) (concatMap findGears x)
+
+gearParts :: String -> [(Int, Int, Int)]
+gearParts str = firstLine ++ middleLines ++ lastLine
+  where
+    lns = lines str
+    numLines = zip [0 ..] lns
+    firstLine = solveLine2' (head lns) (concatMap findGears $ take 2 numLines)
+    middleLines = concatMap solveLine2 (windows numLines)
+    lastLine = solveLine2' (last lns) (concatMap findGears $ take 2 $ reverse numLines)
+
+getGear :: (Int, Int, Int) -> (Int, Int)
+getGear (_, b, c) = (b, c)
+
+getNum :: (Int, Int, Int) -> Int
+getNum (a, _, _) = a
+
+gearRatios :: [(Int, Int, Int)] -> [Int]
+gearRatios [] = []
+gearRatios (p : ps)
+  | null parts = gearRatios ps
+  | otherwise = getNum p * getNum (head parts) : gearRatios ps
+  where
+    parts = filter (\x -> getGear p == getGear x) ps
